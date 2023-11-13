1833ad528f98bf38c36e91b0b53437e587138892
Ahmad Alhasany <ahmadhs271@gmail.com>
2023-11-13 15:08:37 +0300

    last

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -19,9 +19,10 @@ function Grid(props) {
         <Button value = '+' onClick = {() => {props.setExp(pre => {
           if(pre && pre[pre.length-1] !== ' ')
             return pre + ' + '
-          else if (pre){
+          else if (pre)
             return pre.substring(0, pre.length - 3) + ' + ';
-          }
+          else
+          return ''
         })}}/>
         <Button value = {1} onClick = {() => {props.setExp(pre => {return pre + '1'})}}/>
         <Button value = {2} onClick = {() => {props.setExp(pre => {return pre + '2'})}}/>
@@ -29,9 +30,11 @@ function Grid(props) {
         <Button value = '-' onClick = {() => {props.setExp(pre => {
           if(pre && pre[pre.length-1] !== ' ')
             return pre + ' - '
-          else if (pre){
+          else if (pre)
             return pre.substring(0, pre.length - 3) + ' - ';
-          }
+          else
+            return ''
+          
         })}}/>
         <Button value = '.' onClick = {() => {props.setExp(pre => {return pre + '.'})}}/>
         <Button value = {0} onClick = {() => {props.setExp(pre => {if(pre)
@@ -42,16 +45,19 @@ function Grid(props) {
         <Button value = '/' onClick = {() => {props.setExp(pre => {
           if(pre && pre[pre.length-1] !== ' ')
             return pre + ' / '
-          else if (pre){
+          else if (pre)
             return pre.substring(0, pre.length - 3) + ' / ';
-          }
+          else 
+            return ''
+          
         })}}/>
         <Button value = 'x' onClick = {() => {props.setExp(pre => {
           if(pre && pre[pre.length-1] !== ' ')
             return pre + ' * '
-          else if (pre){
+          else if (pre)
             return pre.substring(0, pre.length - 3) + ' * ';
-          }
+          else
+            return ''
         })}}/>
         <Button value = 'RESET' c = 't1 rd' onClick = {() => {props.setExp('')}}/>
         <Button value = '=' c = 't2 e' onClick = {() => {props.setExp(pre => {
